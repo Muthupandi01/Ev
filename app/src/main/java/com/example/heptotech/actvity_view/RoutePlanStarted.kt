a268441bf68bcb9af609fe96fd9acafeb5874e5c
GIT <sudha@heptotechnologies.com>
2024-10-18 10:53:39 +0530

    back arrow set in RoutePlanStarted change by selva

--- app/src/main/java/com/example/heptotech/actvity_view/RoutePlanStarted.kt
+++ app/src/main/java/com/example/heptotech/actvity_view/RoutePlanStarted.kt
@@ -1,7 +1,9 @@
 package com.example.heptotech.actvity_view
 
+import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
+import android.widget.ImageView
 import android.widget.LinearLayout
 import androidx.appcompat.app.AppCompatActivity
 import com.example.heptotech.R
@@ -9,15 +11,24 @@ import com.example.heptotech.R
 class RoutePlanStarted : AppCompatActivity()
 {
     private lateinit var create_Route:LinearLayout
+    private lateinit var back_img: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_routeplan_design)
         create_Route=findViewById(R.id.routeplan_linear)
+        back_img=findViewById(R.id.im)
         create_Route.setOnClickListener()
         {
             val intent = Intent(this, IphoneRouteplan::class.java)
             startActivity(intent)
+        }
+        back_img.setOnClickListener()
+        {
+            setResult(Activity.RESULT_OK)
+            finish()
+
+
         }
 
     }
